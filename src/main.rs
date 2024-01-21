151a3e72a383443f82afde484af02adfb759f11f
Philip J. <kjughx@gmail.com>
2024-01-21 16:11:38 +0100

    Working

--- src/main.rs
+++ src/main.rs
@@ -23,73 +23,41 @@ struct Args {
 
 #[allow(arithmetic_overflow)]
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // let args = Args::parse();
-    //
-    // let mut bus = Bus::new(); // Everyone talks over this
-    //
-    // let memory = Mutex!(Memory::new(MEMORY_SIZE));
-    // let rom = Mutex!(Rom::new(args.load));
-    //
-    // let keyboard = Mutex!(Keyboard::new());
-    // {
-    //     let _keyboard = keyboard.clone();
-    //     std::thread::spawn(|| {
-    //         poll(_keyboard);
-    //     });
-    // }
-    //
-    // let display = Mutex!(Display::new());
-    //
-    // bus.register(memory, MEMORY_START, MEMORY_SIZE)?;
-    // bus.register(keyboard, Addr(0x5000), Addr(0x5001))?;
-    // bus.register(display, Addr(0x5002), Addr(0x5003))?;
-    // bus.register(rom, ROM_START, Addr(ROM_START.0 + ROM_SIZE.0))?;
-    //
-    // let cpu = Mutex!(CPU::new(Mutex!(bus)));
-    // cpu.lock().unwrap().reset();
-    //
-    // if args.visualize {
-    //     visualize::run(cpu)?;
-    // } else {
-    //     loop {
-    //         if !cpu.lock().unwrap().exec() {
-    //             break;
-    //         }
-    //     }
-    //     visualize::run(cpu)?;
-    // }
+    let args = Args::parse();
 
-    let bus = Mutex!(Bus::new());
-    let memory = Mutex!(Memory::new(Addr(0xffff)));
-    bus.lock()
-        .unwrap()
-        .register(memory, Addr(0x0000), Addr(0xffff))
-        .unwrap();
+    let mut bus = Bus::new(); // Everyone talks over this
 
-    for (i, byte) in include_bytes!("hardware/cpu/instructions/arithmetic.bin").iter().enumerate() {
-        bus.lock().unwrap().write(Addr(i as u16), Byte(*byte));
+    let memory = Mutex!(Memory::new(MEMORY_SIZE));
+    let rom = Mutex!(Rom::new(args.load));
+
+    let keyboard = Mutex!(Keyboard::new());
+    {
+        let _keyboard = keyboard.clone();
+        std::thread::spawn(|| {
+            poll(_keyboard);
+        });
     }
 
-    let cpu = Mutex!(CPU::new(bus));
-    cpu.lock().unwrap().set_pc(Addr(0x0400));
+    let display = Mutex!(Display::new());
 
-    let mut instructions = 0;
+    bus.register(memory, MEMORY_START, MEMORY_SIZE)?;
+    bus.register(keyboard, Addr(0x5000), Addr(0x5001))?;
+    bus.register(display, Addr(0x5002), Addr(0x5003))?;
+    bus.register(rom, ROM_START, Addr(ROM_START.0 + ROM_SIZE.0))?;
 
-    loop {
-        if !cpu.lock().unwrap().exec() {
-            break;
+    let cpu = Mutex!(CPU::new(Mutex!(bus)));
+    cpu.lock().unwrap().reset();
+
+    if args.visualize {
+        visualize::run(cpu)?;
+    } else {
+        loop {
+            if !cpu.lock().unwrap().exec() {
+                break;
+            }
         }
-        instructions += 1;
-        // assert!(instructions <= 5475855, "Too many instructions!");
+        visualize::run(cpu)?;
     }
-    dbg!(instructions);
-
-    assert_eq!(
-        cpu.lock().unwrap().get_pc(),
-        Addr(0x0459),
-        "Failure: {:#06X}",
-        cpu.lock().unwrap().get_pc().0
-    );
 
     Ok(())
 }
---
+++ src/routines/tests/setup.s
@@ -0,0 +1,6 @@
+;
+; 6 5 0 2   F U N C T I O N A L   T E S T
+;
+; Practically cut+paste from https://github.com/Klaus2m5/6502_65C02_functional_tests
+;
+
